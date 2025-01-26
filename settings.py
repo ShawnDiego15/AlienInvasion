a42b0d69e4c3dee0b3c0f56ce235c307b798e927
Diego Dominguez <shawndiego15@gmail.com>
2025-01-25 22:13:55 -0600

    Update settings.py
    Added a number of ships.

--- settings.py
+++ settings.py
@@ -12,9 +12,10 @@ class Settings:
 
         # Ship Settings
         self.ship_speed = 1.5
+        self.ship_limit = 3
 
         # Bullet Settings
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
@@ -22,6 +23,6 @@ class Settings:
 
         # Alien Settings
         self.alien_speed = 1.0
-        self.fleet_drop_speed = 10
+        self.fleet_drop_speed = 5
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
